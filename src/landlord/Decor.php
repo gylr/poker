4b7f2d0db2bc46f4c4dba153249956f5ce80fa8a
gylr0505 <gylr0505@163.com>
2023-02-21 12:35:37 +0800

    纸牌花色定义；测试脚本；php限制到最低7.4

---
+++ src/landlord/Decor.php
@@ -0,0 +1,46 @@
+<?php
+// +----------------------------------------------------------------------
+// | poker
+// +----------------------------------------------------------------------
+// | Copyright (c) 2023 All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: gylr0505 <gylr0505@163.com>
+// +----------------------------------------------------------------------
+declare(strict_types=1);
+namespace Lai\Poker\landlord;
+
+class Decor
+{
+    // 黑桃
+    const SPADE = 'S';
+    // 红桃
+    const HEART = 'H';
+    // 方块
+    const DIAMOND = 'D';
+    // 梅花
+    const CLUB = 'C';
+    // 鬼牌
+    const JOKER = 'JOKER';
+
+    /**
+     * 获取花色集合
+     * @return string[]
+     */
+    public static function getAll(): array
+    {
+        return [1=>self::SPADE, self::HEART, self::DIAMOND, self::CLUB];
+    }
+
+    /**
+     * 根据$number取得对应的花色，用在2-A，不含大小王
+     * @param $number
+     * @return string
+     */
+    public static function generateOne($number): string
+    {
+        $fn = fn($fours, $remainder) => $fours[$remainder != 0 ? $remainder : 4];
+        return $fn(self::getAll(), $number % 4);
+    }
+}
---
+++ src/tests/landlord/DecorTest.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+namespace Lai\Poker\tests\landlord;
+
+use Lai\Poker\landlord\Decor;
+use PHPUnit\Framework\TestCase;
+
+class DecorTest extends TestCase
+{
+    public function testGetAll()
+    {
+        $this->assertEquals(4, count(Decor::getAll()),'应该为4种花色');
+    }
+
+    public function testGenerateOne()
+    {
+        $this->assertEquals('S', Decor::generateOne(1),'应该为S');
+        $this->assertEquals('H', Decor::generateOne(2),'应该为H');
+        $this->assertEquals('D', Decor::generateOne(3),'应该为D');
+        $this->assertEquals('C', Decor::generateOne(4),'应该为C');
+
+        $this->assertEquals('S', Decor::generateOne(49),'应该为S');
+        $this->assertEquals('H', Decor::generateOne(50),'应该为H');
+        $this->assertEquals('D', Decor::generateOne(51),'应该为D');
+        $this->assertEquals('C', Decor::generateOne(52),'应该为C');
+    }
+}
